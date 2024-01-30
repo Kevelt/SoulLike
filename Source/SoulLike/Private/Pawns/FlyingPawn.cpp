e06a5f89438e39d5148740494ab5c1a8024c58d7
keveltQ <keveltquintairosesp@gmail.com>
2024-01-30 22:13:52 +0100

    chore: FlyingPawn
    - add Fly input movement
    - add MoveForward input movement
    - add Look input Movement

--- Source/SoulLike/Private/Pawns/FlyingPawn.cpp
+++ Source/SoulLike/Private/Pawns/FlyingPawn.cpp
@@ -3,6 +3,12 @@
 #include "Components/CapsuleComponent.h"
 #include "Components/SkeletalMeshComponent.h"
 #include "UObject/ObjectPtr.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 #include "Pawns/FlyingPawn.h"
 
 // Sets default values
@@ -11,11 +17,24 @@ AFlyingPawn::AFlyingPawn()
 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
+	bUseControllerRotationPitch = true;
+	bUseControllerRotationYaw = true;
+	bUseControllerRotationRoll = false;
+
 	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComponent"));
 	SetRootComponent(CapsuleComponent);
 
 	FlyingSekeletalMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMeshComponent"));
 	FlyingSekeletalMeshComponent->SetupAttachment(GetRootComponent());
+
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+	SpringArmComponent->SetupAttachment(GetRootComponent());
+	SpringArmComponent->TargetArmLength = 300.f;
+	SpringArmComponent->bUsePawnControlRotation = true;
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComponent->SetupAttachment(SpringArmComponent);
+	CameraComponent->bUsePawnControlRotation = false;
 
 	AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
@@ -25,6 +44,58 @@ void AFlyingPawn::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	// Add Input Mapping Context
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* SubSystem = ULocalPlayer::GetSubsystem< UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			SubSystem->AddMappingContext(FlyingMappingContext, 0);
+		}
+	}
+}
+
+void AFlyingPawn::MoveForward(const FInputActionValue& Value)
+{
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if (GetController())
+	{
+		// find out which way is forward
+		const FRotator Rotation = GetController()->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get forward vector
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+
+		// get right vector 
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		// add movement 
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
+
+void AFlyingPawn::Look(const FInputActionValue& Value)
+{
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	if (GetController())
+	{
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
+}
+
+void AFlyingPawn::Fly(const FInputActionValue& Value)
+{
+	const float UpValue = Value.Get<float>();
+
+	if (GetController() && (UpValue != 0.f))
+	{
+		FVector Forward = GetActorUpVector();
+		AddMovementInput(Forward, UpValue);
+	}
 }
 
 // Called every frame
@@ -39,5 +110,11 @@ void AFlyingPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked< UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AFlyingPawn::MoveForward);
+		EnhancedInputComponent->BindAction(FlyAction, ETriggerEvent::Triggered, this, &AFlyingPawn::Fly);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AFlyingPawn::Look);
+	}
 }
 
--- Source/SoulLike/Public/Pawns/FlyingPawn.h
+++ Source/SoulLike/Public/Pawns/FlyingPawn.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "InputActionValue.h"
 #include "FlyingPawn.generated.h"
 
 UCLASS()
@@ -25,10 +26,34 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = InputMapping)
+	TObjectPtr<class UInputMappingContext> FlyingMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = InputMapping)
+	TObjectPtr<class UInputAction> MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = InputMapping)
+	TObjectPtr<UInputAction> LookAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = InputMapping)
+	TObjectPtr<UInputAction> FlyAction;
+
+	void MoveForward(const FInputActionValue& Value);
+
+	void Look(const FInputActionValue& Value);
+
+	void Fly(const FInputActionValue& Value);
+
 private:
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<class UCapsuleComponent> CapsuleComponent;
 
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<class USkeletalMeshComponent> FlyingSekeletalMeshComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<class USpringArmComponent> SpringArmComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<class UCameraComponent> CameraComponent;
 };
