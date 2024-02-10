855c191d60c470ccaf188a4b7dd3f5c0e8fc9eea
keveltQ <keveltquintairosesp@gmail.com>
2024-02-10 21:13:10 +0100

    chore: BaseItemClass
    - add BaseItemClass
    - add OnBeginSphereCollisionOverlap event bind
    - OnEndSphereCollisionOverlap event bind

---
+++ Source/SoulLike/Private/Items/BaseItem.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/BaseItem.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+ABaseItem::ABaseItem()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
+	RootComponent = StaticMeshComponent;
+
+	SphereCollisionComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereCollisionComponent"));
+	SphereCollisionComponent->SetupAttachment(GetRootComponent());
+}
+
+// Called when the game starts or when spawned
+void ABaseItem::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SphereCollisionComponent->OnComponentBeginOverlap.AddDynamic(this, ABaseItem::OnBeginSphereCollisionOverlap);
+	SphereCollisionComponent->OnComponentEndOverlap.AddDynamic(this, ABaseItem::OnEndSphereCollisionOverlap);
+	
+}
+
+void ABaseItem::OnBeginSphereCollisionOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	const FString OtherActorName = OtherActor->GetName();
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 5.f, FColor::Blue, OtherActorName);
+	}
+}
+
+void ABaseItem::OnEndSphereCollisionOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	const FString OtherActorName = OtherActor->GetName();
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 5.f, FColor::Blue, OtherActorName);
+	}
+}
+
+// Called every frame
+void ABaseItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/SoulLike/Public/Items/BaseItem.h
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BaseItem.generated.h"
+
+class UStaticMeshComponent;
+class USphereComponent;
+
+UCLASS()
+class SOULLIKE_API ABaseItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABaseItem();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+protected:
+	template<typename T>
+	T Avg(T First, T Second);
+
+	UFUNCTION()
+	void OnBeginSphereCollisionOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnEndSphereCollisionOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+private:	
+	float CurrentTime;
+
+	UPROPERTY(EditDefaultsOnly)
+	float Amplitude = 0.25f;
+
+	UPROPERTY(EditDefaultsOnly)
+	float TimeConstant = 5.f;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> StaticMeshComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> SphereCollisionComponent;
+
+};
+
+template<typename T>
+inline T ABaseItem::Avg(T First, T Second)
+{
+	return T();
+}
